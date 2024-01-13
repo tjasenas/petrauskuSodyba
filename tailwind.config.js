94dd3c1941e2759429cccde5f359501173c3a581
Vladas Pilipavicius <vpilipavicius1@gmail.com>
2024-01-13 13:34:48 +0200

    new

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,23 @@
 export default {
   content: ["./index.html"],
   theme: {
+    container: {
+      padding: "1rem",
+      center: true,
+    },
+    screens: {
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1200px",
+      // => @media (min-width: 1280px) { ... }
+    },
     extend: {},
   },
   plugins: [],
